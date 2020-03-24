df4fc119832c1100ab503a80310ee087697eda0e
rafadodo <dodoricorafael@gmail.com>
2020-03-24 15:09:51 -0300

    adds MAC calculation to utils

--- utils.py
+++ utils.py
@@ -3,3 +3,23 @@ def printMatrix(a):
         for col in row:
             print("{:.2E}".format(col), end=" ")
         print("")
+        
+def get_MAC(mode_A, mode_B):
+    """Obtain the MAC for the modes defined by
+    column vectors A and B.
+    """
+    MAC = (mode_A.conj().T @ mode_B)**2 / \
+          ((mode_A.conj().T @ mode_A) * (mode_B.conj().T @ mode_B))
+    return MAC
+    
+def get_MAC_matrix(modes_A, modes_B):
+    """Obtain the MAC matrix for the modes represented by
+    the columns of matrices modes_A and modes_B.
+    Rows of the MAC matrix correspond to modes from modes_A.
+    """
+    MAC_matrix = np.zeros((modes_A.shape[1], modes_B.shape[1]))
+    for col_A in range(modes_A.shape[1]):
+        for col_B in range(modes_A.shape[1]):
+            MAC_matrix[col_A, col_B] = get_MAC(modes_A[:, col_A],
+                                               modes_B[:, col_B])
+    return MAC_matrix
