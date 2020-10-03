e8281e1dd50eeac95d8aaf7383751b040ab90c8d
Rafael Dodorico <dodoricorafael@gmail.com>
2020-10-03 02:56:40 -0300

    fixes f06 post processing script and adds ipynb with first modal tests from simulation

--- f06_postpro.py
+++ f06_postpro.py
@@ -13,6 +13,7 @@ def read_nodes_accel(filename):
         lines = f.read().splitlines()
         first_freq = True
         i = 0
+        #
         while i < len(lines):
             if freq_str in lines[i][:17]:
                 freq =  np.append(freq, float(lines[i].split()[2]))
@@ -25,19 +26,29 @@ def read_nodes_accel(filename):
             i += 1
         
         
-        accel_mag = dict.fromkeys(nodes, np.zeros((len(freq),3)))
-        accel_ph = dict.fromkeys(nodes, np.zeros((len(freq),3)))
+        accel_dict = dict.fromkeys(nodes, np.zeros((len(freq),3), dtype=complex))
+        accel_array = np.zeros((len(nodes),len(freq),3), dtype=complex)
         freq_idx = 0
         i = 0
+        # 
         while i < len(lines):
             if freq_str in lines[i][:17]:
                 i += 5
+                node = 0
                 while lines[i][0] != '1':
-                    node = lines[i].split()[1]
-                    accel_mag[node][freq_idx,:]  = np.array(lines[i].split()[3:6])
-                    accel_ph[node][freq_idx,:]  = np.array(lines[i+1].split()[3:6])
-                    i += 1
+                    mag = np.array(lines[i].split()[3:6],
+                                   dtype=float)
+                    phase = np.array(lines[i+1].split()[3:6],
+                                     dtype=float) * np.pi/180
+                    accel_array[node, freq_idx, :]  = mag*np.exp(1j*phase)
+                    i += 2
+                    node += 1
                 freq_idx += 1
             i += 1
+    
+    node_num = 0
+    for node_name in nodes:
+        accel_dict[node_name] = accel_array[node_num,:,:]
+        node_num += 1
 
-    return freq, accel_mag, accel_ph
+    return freq, accel_dict
