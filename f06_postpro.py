c99e1479ac33eb3ecc94c255b15cdbf0ed2f356a
Rafael Dodorico <dodoricorafael@gmail.com>
2020-10-04 17:57:16 -0300

    returns to dict format for accel in f06 postpro

--- f06_postpro.py
+++ f06_postpro.py
@@ -8,6 +8,7 @@ import numpy as np
 def read_nodes_accel(filename):
     freq_str = '      FREQUENCY ='
     freq = np.array([])
+    accel = dict()
     nodes = list()
     with open(filename,'r') as f: 
         lines = f.read().splitlines()
@@ -24,31 +25,24 @@ def read_nodes_accel(filename):
                         nodes.append(lines[i].split()[1])
                         i += 2
             i += 1
-        
-        
-        accel_dict = dict.fromkeys(nodes, np.zeros((len(freq),3), dtype=complex))
-        accel_array = np.zeros((len(nodes),len(freq),3), dtype=complex)
+
+        for node in nodes:
+            accel[node] = np.zeros((len(freq),3), dtype=complex)
         freq_idx = 0
         i = 0
         # 
         while i < len(lines):
             if freq_str in lines[i][:17]:
                 i += 5
-                node = 0
                 while lines[i][0] != '1':
+                    node = lines[i].split()[1]
                     mag = np.array(lines[i].split()[3:6],
                                    dtype=float)
                     phase = np.array(lines[i+1].split()[3:6],
                                      dtype=float) * np.pi/180
-                    accel_array[node, freq_idx, :]  = mag*np.exp(1j*phase)
+                    accel[node][freq_idx, :]  = mag*np.exp(1j*phase)
                     i += 2
-                    node += 1
                 freq_idx += 1
             i += 1
-    
-    node_num = 0
-    for node_name in nodes:
-        accel_dict[node_name] = accel_array[node_num,:,:]
-        node_num += 1
 
-    return freq, accel_dict
+    return freq, accel
