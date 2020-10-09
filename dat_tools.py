f623460cb933e4a450f6f87c144499ca5614445d
Rafael Dodorico <dodoricorafael@gmail.com>
2020-10-09 01:55:38 -0300

    adds modeshape reader for f06 files and fixes bug when reading nastran scientific notation

--- dat_tools.py
+++ dat_tools.py
@@ -1,23 +1,39 @@
 # -*- coding: utf-8 -*-
 """
-
+This module contains functions that extract data from NASTRAN '.dat' output files
 """
-
 import numpy as np
+
+def nastran_float(s):
+    '''Convert a string containing a number in NASTRAN format, that is, with
+    only an '+' or '-' indicating exponential notation, and no 'e', into python
+    format, by adding said 'e'.'''
+    s = s.replace('-','e-')
+    s = s.replace('+','e+')
+    if s[0] == 'e':
+        s = s[1:]
+    return s
 
 def read_node_coords(filename, nodes):
     grid_str = 'GRID'
+    chunk_size = 8
     coords = dict()
     with open(filename,'r') as f: 
         lines = f.read().splitlines()
         i = 0
         #
         while i<len(lines) and len(coords.keys())<len(nodes):
-            line_split = lines[i].split()
+            line = lines[i]
+            line_split = line.split()
             if grid_str in line_split:
                 if line_split[1] in nodes:
-                    coords[line_split[1]] = np.array(lines[i].split()[3:6],
-                                                     dtype=float)
+                    node = line_split[1]
+                    chunks = list()
+                    for c in range(0, len(line), chunk_size):
+                        chunks.append(
+                                nastran_float(line[c:c+chunk_size].strip())
+                                )
+                    coords[node] = np.array(chunks[3:6], dtype=float)
             i += 1
             
     return coords
--- f06_postpro.py
+++ f06_postpro.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-
-
+This module contains functions that extract data from NASTRAN '.f06' output files
 """
 import numpy as np
 
 def read_nodes_accel(filename):
+    """ """
     freq_str = '      FREQUENCY ='
     freq = np.array([])
     accel = dict()
@@ -46,3 +46,42 @@ def read_nodes_accel(filename):
             i += 1
 
     return freq, accel, nodes
+
+def read_modeshapes(filename, nodes):
+    """"Read mode shapes from an f06 file corresponding to a SOL 103 NASTRAN
+    simulation, assuming real displacement is printed."""
+    freq_str = '          CYCLES ='
+    N_DOF = 3 # Degrees of freedom to consider for modeshapes: X Y Z
+    freqs = np.array([])
+    with open(filename,'r') as f:
+        lines = f.read().splitlines()
+        i = 0
+        # Read all modal frequencies
+        while i < len(lines):
+            if lines[i][:18] == freq_str:
+                # Check it is not a repeated frequency
+                if float(lines[i].split()[2]) not in freqs:
+                    freqs =  np.append(freqs, float(lines[i].split()[2]))
+            i += 1
+
+        modeshapes = np.zeros((len(freqs), N_DOF*len(nodes)))
+        freq_idx = 0
+        i = 0
+        # Read mode shape for each frequency
+        while i < len(lines):
+            if lines[i][:18] == freq_str and freq_idx<len(freqs):
+                # Check it is the currently expected frequency
+                if float(lines[i].split()[2]) == freqs[freq_idx]:
+                    i += 3
+                    dof = 0
+                    while lines[i][0] != '1':
+                        node = lines[i].split()[0]
+                        if node in nodes:
+                            node_disp = np.array(lines[i].split()[2:5], dtype=float)
+                            modeshapes[freq_idx, dof:dof+N_DOF]  = node_disp
+                            dof += N_DOF
+                        i += 1
+                    freq_idx += 1
+            i += 1
+
+    return freqs, modeshapes
