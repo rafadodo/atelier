0f5160f9ea67e5da0db91057ae5044bb99775bec
rafadodo <dodoricorafael@gmail.com>
2025-03-21 11:24:24 -0300

    lowercase for mac functions, adjust update management for mac plots

--- mac.py
+++ mac.py
@@ -4,15 +4,15 @@ This module contains functions that assist in dealing with MAC (Modal Assurance 
 and matrices.
 
 Functions:
-- get_MAC(mode_A, mode_B): Computes the MAC value between two mode shape vectors.
-- get_MAC_matrix(modes_A, modes_B): Computes the MAC matrix between two sets of mode shapes.
-- plot_MAC(MAC, color_map, text_color, title_str, labels):   Plots a given MAC matrix.
+- get_mac_value(mode_A, mode_B): Computes the MAC value between two mode shape vectors.
+- get_mac_matrix(modes_A, modes_B): Computes the MAC matrix between two sets of mode shapes.
+- plot_mac(mac_matrix, color_map, text_color, title_str, labels): Plots a given MAC matrix.
 """
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def get_MAC(mode_A, mode_B):
+def get_mac_value(mode_A, mode_B):
     """Obtain the MAC for the mode shapes defined by column vectors A and B.
 
     Parameters:
@@ -22,12 +22,13 @@ def get_MAC(mode_A, mode_B):
     Returns:
         float: The MAC value
     """
-    MAC = abs(mode_A.T @ mode_B.conj())**2 / \
+    mac = abs(mode_A.T @ mode_B.conj())**2 / \
           (abs(mode_A.T @ mode_A.conj()) * abs(mode_B.T @ mode_B.conj()))
-    return MAC
+
+    return mac
 
 
-def get_MAC_matrix(modes_A, modes_B):
+def get_mac_matrix(modes_A, modes_B):
     """Compute the MAC matrix for two sets of mode shapes.
 
     Parameters:
@@ -38,15 +39,16 @@ def get_MAC_matrix(modes_A, modes_B):
         numpy.ndarray: A MAC matrix where each element (i, j) represents the MAC value between
                        mode i from modes_A and mode j from modes_B.
     """
-    MAC_matrix = np.zeros((modes_A.shape[1], modes_B.shape[1]))
+    mac_matrix = np.zeros((modes_A.shape[1], modes_B.shape[1]))
     for col_A in range(modes_A.shape[1]):
         for col_B in range(modes_B.shape[1]):
-            MAC_matrix[col_A, col_B] = get_MAC(modes_A[:, col_A],
+            mac_matrix[col_A, col_B] = get_mac_value(modes_A[:, col_A],
                                                modes_B[:, col_B])
-    return MAC_matrix
+
+    return mac_matrix
 
 
-def plot_MAC(MAC, color_map, text_color, title_str='MAC', labels=['', '']):
+def plot_mac(mac_matrix, color_map, text_color, title_str='MAC', labels=['Mode', 'Mode']):
     """Plot a given Modal Assurance Criterion (MAC) matrix.
 
     Parameters:
@@ -60,15 +62,21 @@ def plot_MAC(MAC, color_map, text_color, title_str='MAC', labels=['', '']):
         matplotlib.axes.Axes: The axes object of the resulting plot.
     """
     fig, ax = plt.subplots()
-    cax = ax.matshow(MAC, cmap=color_map)
+    cax = ax.matshow(mac_matrix, cmap=color_map)
     plt.title(title_str, y=1.15)
     plt.ylabel(labels[0])
     plt.xlabel(labels[1])
     ax.xaxis.set_label_position('top')
-    ticks_lst = ['']+list(np.arange(MAC.shape[0])+1)
-    ax.set_xticklabels(ticks_lst)
-    ax.set_yticklabels(ticks_lst)
-    plt.tight_layout()
-    for (i, j), z in np.ndenumerate(MAC):
+
+    ticks = np.arange(mac_matrix.shape[0])
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+    ax.set_xticklabels(ticks + 1)
+    ax.set_yticklabels(ticks + 1)
+
+    for (i, j), z in np.ndenumerate(mac_matrix):
         ax.text(j, i, '{:.2f}'.format(z), ha='center', va='center', color=text_color)
+
+    plt.tight_layout()
+
     return ax
